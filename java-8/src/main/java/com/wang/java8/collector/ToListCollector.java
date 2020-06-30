8dc80add2d97000e27a0043bb4587e17a0959eb0
cuiweiman <cuiweiman1996@163.com>
2020-06-30 15:37:27 +0800

    'collector完结'

---
+++ java-8/src/main/java/com/wang/java8/collector/ToListCollector.java
@@ -0,0 +1,53 @@
+package com.wang.java8.collector;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * @description: 自定义Collector接口方法
+ * @author: weiman cui
+ * @date: 2020/6/30 14:58
+ */
+public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
+
+    private void log(final String log) {
+        System.out.println(Thread.currentThread().getName() + "：" + log);
+    }
+
+    @Override
+    public Supplier<List<T>> supplier() {
+        log("supplier");
+        return ArrayList::new;
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        log("accumulator");
+        return List::add;
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        log("combiner");
+        return (list1, list2) -> {
+            list1.addAll(list2);
+            return list1;
+        };
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        log("finisher");
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        log("characteristics");
+        return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH, Characteristics.CONCURRENT));
+    }
+}
---
+++ java-8/src/main/java/com/wang/java8/collector/ToListCollectorTest.java
@@ -0,0 +1,27 @@
+package com.wang.java8.collector;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+
+/**
+ * @description: 测试自定义的Collector方法
+ * @author: weiman cui
+ * @date: 2020/6/30 15:30
+ */
+public class ToListCollectorTest {
+
+    public static void main(String[] args) {
+        Collector<String, List<String>, List<String>> collector = new ToListCollector<>();
+        String[] array = new String[]{"Alex", "syy", "Hello", "lambda", "collector", "java8", "stream"};
+        /*List<String> result = Arrays.asList(array).parallelStream()
+                .filter(ele -> ele.length() > 5)
+                .collect(collector);*/
+        List<String> result = Arrays.stream(array)
+                .filter(ele -> ele.length() > 5)
+                .collect(collector);
+        System.out.println(result);
+
+    }
+
+}
