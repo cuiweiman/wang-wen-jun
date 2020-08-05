01fa5522244a88979e8054f4f8aae3c2207bf25c
cuiweiman <daocaoren22222@163.com>
2020-08-05 23:56:56 +0800

    'CharSink与CharSource'

---
+++ google-guava/src/test/java/com/wang/guava/io/CharSinkTest.java
@@ -0,0 +1,23 @@
+package com.wang.guava.io;
+
+import com.google.common.io.CharSink;
+import org.junit.Test;
+
+/**
+ * @description: CharSink 字符流  输出
+ * @date: 2020/8/5 23:45
+ * @author: wei·man cui
+ */
+public class CharSinkTest {
+
+    /**
+     * CharSource 相当于 Reader
+     * CharSink 相当于 Writer
+     */
+    @Test
+    public void testCharSink() {
+        // 给个对象，看源码
+        CharSink charSink;
+    }
+
+}
---
+++ google-guava/src/test/java/com/wang/guava/io/CharSourceTest.java
@@ -0,0 +1,46 @@
+package com.wang.guava.io;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.CharSink;
+import com.google.common.io.CharSource;
+import com.google.common.io.Files;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * @description: CharSource 字符流 读取
+ * @date: 2020/8/5 23:32
+ * @author: wei·man cui
+ */
+public class CharSourceTest {
+
+    /**
+     * CharSource 基本方法
+     */
+    @Test
+    public void testCharSourceWrap() throws IOException {
+        CharSource charSource = CharSource.wrap("i me the CharSource");
+        String read = charSource.read();
+        assertThat(read, equalTo("i me the CharSource"));
+        ImmutableList<String> lines = charSource.readLines();
+        assertThat(lines.size(), equalTo(1));
+        assertThat(charSource.length(), equalTo(19L));
+        assertThat(charSource.lengthIfKnown().get(), equalTo(19L));
+    }
+
+    @Test
+    public void testConcat() throws IOException {
+        CharSource charSource = CharSource.concat(
+                CharSource.wrap("i me the CharSource 1"),
+                CharSource.wrap("i me the CharSource 2"),
+                CharSource.wrap("i me the CharSource 3")
+        );
+        charSource.lines().forEach(System.out::println);
+    }
+
+
+}
