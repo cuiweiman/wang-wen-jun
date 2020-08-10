65cac7ce21a9c0a374b1cc4887c16cd0e571453c
cuiweiman <daocaoren22222@163.com>
2020-08-10 23:27:02 +0800

    '完善EventBus监听'

--- google-guava/src/main/java/com/wang/guava/eventbus/monitor/MonitorClient.java
+++ google-guava/src/main/java/com/wang/guava/eventbus/monitor/MonitorClient.java
@@ -2,6 +2,10 @@ package com.wang.guava.eventbus.monitor;
 
 import com.google.common.eventbus.EventBus;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @description: 监听文件目录 中的文件和文件夹是否发生改变
  * @date: 2020/8/10 23:12
@@ -13,6 +17,17 @@ public class MonitorClient {
         bus.register(new FileChangeListener());
         String path = "E:\\ideaWorkspace\\wang-wen-jun\\google-guava";
         TargetMonitor monitor = new DirectoryTargetMonitor(bus, path);
+
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.schedule(() -> {
+            try {
+                monitor.stopMonitor();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 2, TimeUnit.MINUTES);
+        executorService.shutdown();
+        // 启动后，只能通过其他线程来关闭监听。这里开启了一个 定时线程 来关闭。
         monitor.startMonitor();
     }
 }
