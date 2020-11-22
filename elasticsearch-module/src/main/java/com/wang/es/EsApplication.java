806bd4e34d5956711f9b42109caff8aa83414f05
cuiweiman <daocaoren22222@163.com>
2020-11-22 11:04:36 +0800

    'init'

--- elasticsearch-module/src/main/java/com/wang/es/EsApplication.java
+++ elasticsearch-module/src/main/java/com/wang/es/EsApplication.java
@@ -1,10 +1,16 @@
 package com.wang.es;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @description: ES启动类
  * @date: 2020/11/19 21:48
  * @author: wei·man cui
  */
-
+@SpringBootApplication
 public class EsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EsApplication.class, args);
+    }
 }
