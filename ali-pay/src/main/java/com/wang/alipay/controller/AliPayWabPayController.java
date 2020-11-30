2914d4bce8ddf831a8e497daec669f17d9a2d382
cuiweiman <daocaoren22222@163.com>
2020-11-30 17:07:08 +0800

    feat: 🎸 刚打开，需求来，高需求

---
+++ ali-pay/src/main/java/com/wang/alipay/controller/AliPayWabPayController.java
@@ -0,0 +1,44 @@
+package com.wang.alipay.controller;
+
+import com.alipay.api.AlipayClient;
+import com.wang.alipay.component.AliPayComponent;
+import com.wang.alipay.properties.AliPayProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+/**
+ * @description: 阿里支付 手机H5页面支付
+ * @author: wei·man cui
+ * @date: 2020/11/30 15:25
+ */
+@Slf4j
+@Controller
+@RequestMapping("/wabPay")
+public class AliPayWabPayController {
+
+    @Resource
+    private AlipayClient alipayClient;
+
+    @Resource
+    private AliPayProperties aliPayProperties;
+
+    @Resource
+    private AliPayComponent aliPayComponent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
