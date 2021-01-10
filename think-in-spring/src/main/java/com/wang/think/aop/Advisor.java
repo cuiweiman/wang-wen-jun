b39ef50a73a4b79a2bc379609f0c6aa918e4b838
cuiweiman <daocaoren22222@163.com>
2021-01-10 23:33:59 +0800

    feat(aop): spring-aop 使用示例

---
+++ think-in-spring/src/main/java/com/wang/think/aop/Advisor.java
@@ -0,0 +1,43 @@
+package com.wang.think.aop;
+
+import com.sun.corba.se.spi.ior.ObjectKey;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+/**
+ * @description: 动态aop  使用示例
+ * @date: 2021/1/10 23:19
+ * @author: wei·man cui
+ */
+@Aspect
+public class Advisor {
+
+    @Pointcut("execution(* *.test(..))")
+    public void test() {
+
+    }
+
+    @Before("test()")
+    public void before() {
+        System.out.println("before test");
+    }
+
+    @After("test()")
+    public void after() {
+        System.out.println("after test");
+    }
+
+    @Around("test()")
+    public Object around(ProceedingJoinPoint point) {
+        System.out.println("around before");
+        Object o = null;
+        try {
+            o = point.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        System.out.println("around after");
+        return o;
+    }
+
+}
