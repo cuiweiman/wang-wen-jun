4c62daee71681bf82b4fef2359d80c3b0c0c3376
cuiweiman <daocaoren22222@163.com>
2021-01-20 15:56:45 +0800

    docs: ✏️ 对 BeanPostProcessor后处理器 新的认识和理解

--- think-in-spring/src/main/java/com/wang/think/lifecycle/MyBeanPostProcessor.java
+++ think-in-spring/src/main/java/com/wang/think/lifecycle/MyBeanPostProcessor.java
@@ -4,7 +4,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 /**
- * @description: bean 生命周期 的 后置函数
+ * 针对 Spring 容器中，所有的 bean 生效。
+ * {@link #postProcessBeforeInitialization(Object, String)}：bean 调用构造函数实例化完成、依赖注入完成，在调用自定义初始化函数之前，触发
+ * {@link #postProcessAfterInitialization(Object, String)}：bean 调用构造函数实例化完成、依赖注入完成，在调用自定义初始化函数结束后，触发
+ *
+ * @description: bean 生命周期 的 后处理器
  * @author: wei·man cui
  * @date: 2020/12/30 17:38
  */
