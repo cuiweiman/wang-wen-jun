bffef5fdc708b9b324c7f729764c099c0e10e680
cuiweiman <daocaoren22222@163.com>
2021-04-21 23:36:58 +0800

    docs: 玩游戏了……诶呀呀，没搞完，

--- netty-basic/src/main/java/com/wang/netty/daxin/chapter02/NioChannel.java
+++ netty-basic/src/main/java/com/wang/netty/daxin/chapter02/NioChannel.java
@@ -1,9 +1,65 @@
 package com.wang.netty.daxin.chapter02;
 
+import com.wang.netty.daxin.chapter01.EventLoop2;
+import com.wang.netty.daxin.chapter01.MyChannel;
+import com.wang.netty.daxin.chapter01.nettymodel.HandlerContext;
+import com.wang.netty.daxin.chapter01.nettymodel.MyHandler1;
+import com.wang.netty.daxin.chapter01.nettymodel.MyHandler2;
+import com.wang.netty.daxin.chapter01.nettymodel.Pipeline;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
- * @description: 处理 NioEventLoop2 分发过来的 读写事件
+ * 负责 处理 IO的 读写 事件，将 NioEventLoop 的读写事件进行解耦，NioEventLoop 只进行事件分发 和 监听事件的注册
+ * <p>
+ * 读数据 事件：NioEventLoop 监听到 可读事件后，传送给 NioChannel。
+ * {@link NioChannel#read} 将数据 经过方法 {@link HandlerContext#fireChannelRead} 传递给
+ * 链式调用的处理器 {@link MyHandler1#channelRead}进行链式处理调用，在链式处理过程中，通过
+ * {@link HandlerContext#fireChannelRead} 方法 进行 消息的链式传递。{@link Pipeline} 中维护了链式处理器的顺序。
+ *
+ * <p>
+ * 写数据 事件： NioEventLoop 监听到 可写事件后，传送给 NioChannel。
+ * 1. 调用 {@link NioChannel#doWrite}触发写事件，经由方法 {@link HandlerContext#write} 传递给 链式调用的
+ * 处理器 {@link MyHandler2#write}进行链式处理调用，并经过 {@link HandlerContext#write} 方法有 tail 节点向 head 节点
+ * 传递写数据。
+ * 2. 写数据最终到达 {@link Pipeline.PipelineHandler#write} 方法中，将 写数据 添加到 ByteBuffer 缓冲队列中。
+ * 3. {@link MyHandler2#flush} 方法 依次从 tail 节点 传递到 head 节点，最终到达 {@link Pipeline.PipelineHandler#flush} 方法，
+ * 在该方法中 触发了 {@link MyChannel#doFlush()} 方法，将 当前的 selector 监听事件 改成了 OP_WRITE 可写事件。
+ * 4. {@link EventLoop2} 监听到 可写事件 后，将消息分发给 {@link MyChannel#write} 方法，最终将消息写入到 Channel 中。
+ *
+ * @description: 处理 NioEventLoop 分发过来的 读写事件
  * @author: wei·man cui
  * @date: 2021/4/21 18:06
  */
 public class NioChannel {
+
+    private SocketChannel channel;
+
+    private NioEventLoop eventLoop;
+
+
+    /**
+     * 写消息队列，需要向 channel 中写入消息时，放入队列即可
+     */
+    private Queue<ByteBuffer> writeQueue = new ArrayBlockingQueue<>(16);
+
+    private NioPipeline pipeline;
+
+
+    public NioChannel(SocketChannel channel, NioEventLoop eventLoop) {
+        this.channel = channel;
+        this.eventLoop = eventLoop;
+
+        // this.pipeline = new NioPipeline(this, eventLoop);
+        // this.pipeline.addLast(new MyHandler1());
+        // this.pipeline.addLast(new MyHandler2());
+    }
+
+    public void doWrite(Object msg) {
+        // this.pipeline.tailCtx.write(msg);
+    }
+
 }
--- netty-basic/src/main/java/com/wang/netty/daxin/chapter02/NioEventLoop.java
+++ netty-basic/src/main/java/com/wang/netty/daxin/chapter02/NioEventLoop.java
@@ -1,9 +1,104 @@
 package com.wang.netty.daxin.chapter02;
 
+import java.io.IOException;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * @description: 管理 监听读写事件 的注册，并对 监听到的 读写事件 进行分发
  * @author: wei·man cui
  * @date: 2021/4/21 18:06
  */
-public class NioEventLoop {
+public class NioEventLoop implements Runnable {
+
+    public Selector selector;
+
+    private final Thread thread;
+
+    private final Queue<Runnable> taskQueue = new LinkedBlockingQueue<>(32);
+
+
+    public NioEventLoop() {
+        try {
+            selector = SelectorProvider.provider().openSelector();
+            thread = new Thread(this);
+            thread.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 将 channel 的 监听事件 注册到 selector 中。
+     * <p>
+     * 在对 selector 进行事件注册时，{@code int eventNum = selector.select();}
+     * 使得 线程发生了阻塞，无法 使用本线程 继续 进行事件的注册。
+     * <p>
+     * 解决方法：
+     * 将注册 任务放在 taskQueue 队列中，使用 本类中的 thread 处理 线程任务。
+     * <p>
+     * {@link Selector#wakeup()} 使{@link NioEventLoop#run}中尚未返回的第一个选择操作立即返回。
+     * 如果另一个线程正阻塞在 select() 或 select(long) 方法的调用中，则调用本方法将其立即返回。
+     *
+     * @param channel channel
+     * @param keyOps  监听的事件
+     */
+    public void register(SocketChannel channel, int keyOps) {
+        // 将 注册监听事件 的任务 封装到 线程任务中
+        taskQueue.add(() -> {
+            try {
+                NioChannel nioChannel = new NioChannel(channel, this);
+                final SelectionKey selectionKey = channel.register(selector, keyOps);
+                // 附加 NioChannel 对象，其中保存了 SocketChannel 和 当前 NioEventLoop 对象
+                // 便于 NioEventLoop 事件监听 和 读写分发 的解耦
+                // selectionKey.attachment(nioChannel);
+            } catch (ClosedChannelException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        // 唤醒 下面 run() 线程中 Selector#select 方法阻塞的 线程
+        // 使得 该线程继续向下执行，遍历 taskQueue 队列，完成 监听事件的注册
+        selector.wakeup();
+    }
+
+    @Override
+    public void run() {
+        // 死循环：Thread 处于 非中断 状态
+        while (!Thread.interrupted()) {
+            try {
+                System.out.println(thread + " 开始轮询监听 IO 事件");
+                // 本方法 会发生 线程 阻塞
+                int eventNum = selector.select();
+                if (eventNum > 0) {
+                    // 发生了 IO 事件
+                    System.out.println("[服务器]发生了IO事件，数量：" + eventNum);
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey selectionKey = iterator.next();
+                        NioChannel nioChannel = (NioChannel) selectionKey.attachment();
+                        if (selectionKey.isReadable()) {
+                            // nioChannel.read(selectionKey);
+                        } else if (selectionKey.isWritable()) {
+                            // nioChannel.write(selectionKey);
+                        }
+                        iterator.remove();
+                    }
+                }
+                Runnable task;
+                while (Objects.nonNull(task = taskQueue.poll())) {
+                    // 普通方法 的方式调用
+                    task.run();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
